dccde093f7f4e0bee3fea09feb417d9732515d29
jmercad0 <lakers722@gmail.com>
2018-10-02 21:15:20 -0700

    Initial thoughts for Customer class

---
+++ Customer.java
@@ -0,0 +1,17 @@
+
+public class Customer {
+	private int arrivalTime;
+	public boolean successfulSale;
+	public Customer(int theArrivalTime) {
+		arrivalTime = theArrivalTime;
+	}
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setSuccessfulSale(boolean sale) {
+		successfulSale = sale;
+	}
+	public boolean isSaleSuccessful() {
+		return successfulSale;
+	}
+}
