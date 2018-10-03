93c0e105dd3e23ea50c71c77b74645f769acbf8b
jmercad0 <lakers722@gmail.com>
2018-10-03 11:20:49 -0700

    Added slight functionality to printSeatingChart
    also added 2d-array of seats which you can pull seller name and customer count from (to use for printing chart)

--- Seats.java
+++ Seats.java
@@ -1,21 +1,33 @@
 
 public class Seats {
-	boolean[][] seats;
+	boolean[][] availableSeats;
+	Seat[][] seats;
 	public Seats() {
-		seats = new boolean[10][10];
+		availableSeats = new boolean[10][10];
+		seats = new Seat[10][10];
+		for (int i = 0; i<10; i++) {
+			for (int j = 0; j<10;j++) {
+				seats[i][j] = new Seat(null,i,j);
+			}
+		}
 	}
 	public boolean[][] getSeatingChart() {
-		return seats;
+		return availableSeats;
 	}
-	public void setSold(int i, int j) {
-		seats[i][j] = true;
+	public void setSold(TicketSeller seller, int i, int j) {
+		availableSeats[i][j] = true;
+		seats[i][j].setSeller(seller);
 	}
 	public void printSeatingChart() {
 		for (int i = 0;i<10;i++) {
 			for (int j = 0;j<10;j++) {
-				
+				if (!availableSeats[i][j]) {
+					System.out.print("[_]  ");
+				}else {
+					System.out.print(seats[i][j].getSellerName()+seats[i][j].returnCustCount()+" ");
+				}
 			}
-			
+			System.out.println();
 		}
 	}	
 }
