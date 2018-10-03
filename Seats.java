b8425d874b9844e85faf2df1ada6890f4d48279f
jmercad0 <lakers722@gmail.com>
2018-10-03 11:46:34 -0700

    restore back to skeleton

--- Seats.java
+++ Seats.java
@@ -1,33 +1,21 @@
 
 public class Seats {
-	boolean[][] availableSeats;
-	Seat[][] seats;
+	boolean[][] seats;
 	public Seats() {
-		availableSeats = new boolean[10][10];
-		seats = new Seat[10][10];
-		for (int i = 0; i<10; i++) {
-			for (int j = 0; j<10;j++) {
-				seats[i][j] = new Seat(null,i,j);
-			}
-		}
+		seats = new boolean[10][10];
 	}
 	public boolean[][] getSeatingChart() {
-		return availableSeats;
+		return seats;
 	}
-	public void setSold(TicketSeller seller, int i, int j) {
-		availableSeats[i][j] = true;
-		seats[i][j].setSeller(seller);
+	public void setSold(int i, int j) {
+		seats[i][j] = true;
 	}
 	public void printSeatingChart() {
 		for (int i = 0;i<10;i++) {
 			for (int j = 0;j<10;j++) {
-				if (!availableSeats[i][j]) {
-					System.out.print("[_]  ");
-				}else {
-					System.out.print(seats[i][j].getSellerName()+seats[i][j].returnCustCount()+" ");
-				}
+				
 			}
-			System.out.println();
+			
 		}
 	}	
 }
