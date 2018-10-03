e7561b4adcc2a16e5dc5059a947695a6ba9105ef
richsensations <32314978+richsensations@users.noreply.github.com>
2018-10-03 11:56:41 -0700

    Changed TicketSeller to interface and added the three ticket seller classes.

---
+++ HighPricedTicketSeller.java
@@ -0,0 +1,40 @@
+
+public class HighPricedTicketSeller implements TicketSeller{
+
+	@Override
+	public boolean checkAvailableSeats(boolean[][] seats) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void assignSeatToCustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sell() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkIfSeatIsAssigned() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int returnProcTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setProcTime(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
