e527c047ba9482ac5c234aba077a4de021ac31c2
jmercad0 <lakers722@gmail.com>
2018-10-02 21:18:36 -0700

    Initial thoughts/skeleton for Seats class
    10x10 boolean array

---
+++ Seats.java
@@ -0,0 +1,21 @@
+
+public class Seats {
+	boolean[][] seats;
+	public Seats() {
+		seats = new boolean[10][10];
+	}
+	public boolean[][] getSeatingChart() {
+		return seats;
+	}
+	public void setSold(int i, int j) {
+		seats[i][j] = true;
+	}
+	public void printSeatingChart() {
+		for (int i = 0;i<10;i++) {
+			for (int j = 0;j<10;j++) {
+				
+			}
+			
+		}
+	}	
+}
