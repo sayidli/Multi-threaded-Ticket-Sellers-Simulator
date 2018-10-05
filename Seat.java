b527e61b73ea15e248ac6484c9f767c162f3a251
vinhvuu224 <42885094+vinhvuu224@users.noreply.github.com>
2018-10-05 04:25:14 -0700

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,28 +1,60 @@
-
-public class Seat {
-	private String assignmentID;
-	private boolean isSold;
-	
-	public Seat(){
-		assignmentID = "";
-		isSold = false;
-	}
-	
-	public boolean getIsSold(){
-		return isSold;
-	}
-	
-	public void setSold(){
-		isSold = true;
-	}
-	
-	
-	public void setAssignmentID(String id){
-		assignmentID = id;
-	}
-	
-	public String getAssignmentID(){
-		return assignmentID;
-	}
-	
+public class SeatingMap {
+		
+		
+		private String assignmentID;
+		private boolean isSold;
+		
+//		public Seat(){
+//			assignmentID = "";
+//			isSold = false;
+//		}
+//		
+		
+		boolean[][] seats;
+		
+		public void setSold(int i, int j) {
+			seats[i][j] = true;
 }
+		
+		
+		public SeatingMap() {
+			seats = new boolean[10][10];
+		}
+		
+			public boolean available(int i, int j){
+				return !seats[i][j];
+			}
+		
+		public boolean[][] getSeatingChart() {
+			return seats;
+		}
+		
+		public void printSeatingChart() {
+			int rows=seats.length;
+			int columns=seats[0].length;
+			for(int i=0;i<rows; i++){
+				for(int j=0; j<columns; j++){
+					System.out.print(seats[i][j]+"   ");
+				}
+				System.out.println();
+			}	
+			}
+		
+		public boolean getIsSold(){
+			return isSold;
+		}
+		
+		public void setSold(){
+			isSold = true;
+		}
+		
+		
+		public void setAssignmentID(String id){
+			assignmentID = id;
+		}
+		
+		public String getAssignmentID(){
+			return assignmentID;
+		}
+		
+	}
