3bacddd350bc9577fe6c9a8fd8d06e422040d19e
jmercad0 <lakers722@gmail.com>
2018-10-05 00:17:20 -0700

    implemented checkAvailableSeats & saveCoordinates to be used when assigning seats to customer

--- HighPricedTicketSeller.java
+++ HighPricedTicketSeller.java
@@ -4,16 +4,32 @@ public class HighPricedTicketSeller extends TicketSeller{
 
 	private PriorityQueue<Customer> customerQueue;
 	private Seat[][] seatingMap;
-	
+	private int rowStart = 0;
+	private int availRow;
+	private int availCol;
 	public HighPricedTicketSeller(int numOfCustomers, Seat[][] seatingMap){
 		customerQueue = CustomerGenerator.createCustomers(numOfCustomers);
 		this.seatingMap = seatingMap;
 	}
-	
 	@Override
-	public boolean checkAvailableSeats(boolean[][] seats) {
-		// TODO Auto-generated method stub
+	public boolean checkAvailableSeats(Seat[][] seats) {
+		// starts searching 0'th row
+		for (int i = rowStart;i<10;i--) {
+			for (int j=0;j<10;j++) {
+				// saves coordinates of first available seat and returns true
+				if (!seats[i][j].getIsSold()) {
+					saveCoordinates(i,j);
+					return true;
+				}
+			}
+		}
+		//returns false if not seats are available
 		return false;
+	}
+	//helper method to save coordinates to use when assigning seat to custoemr
+	public void saveCoordinates(int row, int col) {
+		availRow = row;
+		availCol = col;
 	}
 
 	@Override
